351655e9e24b71f9f935ea6158b08d3f8e085de8
ddarak00 <97087246+ddarak00@users.noreply.github.com>
2022-01-19 02:04:19 -0800

    Changed size of textfield.  Added a margin.  All in Top of BorderPane.
    Next, make text in Textfield disappear when TextField is in focus.

--- FxRules/src/application/Main.java
+++ FxRules/src/application/Main.java
@@ -1,9 +1,14 @@
 package application;
 	
+//import com.sun.prism.paint.Color;
+
 import javafx.application.Application;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.scene.layout.BorderPane;
+import javafx.geometry.*;
 
 
 public class Main extends Application {
@@ -11,8 +16,16 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,1000,300);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("Rules for Life");
+			
+			
+			TextField b = new TextField("Enter Life Rule");
+			b.setMaxWidth(900);
+			BorderPane.setMargin(b, new Insets(50));
+			BorderPane.setAlignment(b, Pos.CENTER);
+			root.setTop(b);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
